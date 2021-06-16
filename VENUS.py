1258ac71f60977733c2d0787c5aa3ee9fd40d71d
Cheyu Lee <cheyu@apple-silicon.lan>
2021-06-15 19:04:30 -0700

    Add argparser

--- VENUS.py
+++ VENUS.py
@@ -1,6 +1,15 @@
-import sys
 import argparse
-import os
-import subprocess
-import os.path
-import fileinput
+
+usage = """Usage: %prog[-h] -r1 cDNA.read.fastq -r2 barcode.read.fastq
+                  -index Index Directory -scBarcodeWhitelist Whitelist file
+                  -o Output File Name Prefix -t 16
+                  -scCBstart 1 -scCBlen 16 -scUMIstart 17 -scUMIlen 10"""
+
+def main():
+    parser = argparse.ArgumentParser(description='VENUS')
+
+    return None
+
+
+if __name__ == '__main__':
+    main()
