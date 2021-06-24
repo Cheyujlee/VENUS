fadaedea98f1961d09d144336301b2ee041fe573
Cheyu Lee <cheyu@apple-silicon.lan>
2021-06-24 15:50:03 -0700

    add working bulk RNA-seq code

--- VENUS.py
+++ VENUS.py
@@ -1,11 +1,28 @@
 import argparse
 import os
-import sys
 
-def main():
-    print("executed")
+### Creates the Argument Parser object ###
+parser = argparse.ArgumentParser(description="subtractive analysis of viral bulk single-end RNA-seq")
 
-    return None
 
-if __name__ == "__main__":
-    main()
+### Specifies the argument for Bulk Single-end RNA-seq ###
+parser.add_argument("--read1", type=str, required=True,
+                    help="read1 of bulk single-end RNA-seq")
+parser.add_argument("--indexDir", type=str, required=True,
+                    help="genome index directory")
+parser.add_argument("--outDir", type=str, required=False, 
+                    help="directory to store output")
+args = parser.parse_args()
+
+
+### Concatenate and run command in bash ###
+cmd="STAR " + \
+    "--runThreadN 16 " + \
+    "--outFileNamePrefix " + args.outDir + " " \
+    "--genomeDir " + args.indexDir + " " \
+    "--readFilesIn " + args.read1 + " "
+print("Running " + cmd)
+os.system(cmd)
+
+### local test ###
+# print("{} is read1 and {} is read2; {} is the genomeDir".format(args.read1, args.read2, args.genomeDir))
