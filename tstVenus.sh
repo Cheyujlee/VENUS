8bddf275ff0da44503b4abefee704022fa7c1df3
Cheyu Lee <cheyu@vcv064124.vpn.uci.edu>
2021-06-25 10:29:04 -0700

    ADD updated tstVenus.sh testing script

--- tstVenus.sh
+++ tstVenus.sh
@@ -5,9 +5,8 @@
 #SBATCH --mem-per-cpu=4G
 #SBATCH --time=24:00:00
 #SBATCH --partition=zhanglab.p
-#SBATCH --output=/srv/disk00/cheyul1/Venus/06-24-21/output1/tstVenus.log
+#SBATCH --output=/srv/disk00/cheyul1/Venus/06-25-21/run1/tstVenus.log
 
 python3 /srv/disk00/cheyul1/Venus/Venus.py \
 --read1 /srv/disk00/cheyul1/Venus/bulk_data/DRR193865_1.fastq \
---indexDir /srv/disk00/cheyul1/elxcessSTAR-2.7.8a/indices/HIV.genomeDir/ \
---outDir /srv/disk00/cheyul1/Venus/06-24-21/output1/
+--outDir /srv/disk00/cheyul1/Venus/06-25-21/run1/
