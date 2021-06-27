f5e5bb05827ce28c215d136aa6e0e61a2d8e1755
Cheyu Lee <cheyu@vcv079141.vpn.uci.edu>
2021-06-27 15:28:22 -0700

    ADD single-cell mapping capability

--- Venus.py
+++ Venus.py
@@ -6,28 +6,43 @@ import sys
 # This is the current path to the default human genome index directory
 human_indexDir = "/srv/disk00/cheyul1/excessSTAR-2.7.8a/indices/human.genomeDir/"
 # This is the current path to the default mega virus index directory
-virus_indexDir = "/srv/disk00/cheyul1/excessSTAR-2.7.8a/indices/virus.genomeDir/"
+virus_indexDir = "/srv/disk00/cheyul1/excessSTAR-2.7.8a/indices/HIV.genomeDir/"
 
 
 ### Creates the Argument Parser object ###
-parser = argparse.ArgumentParser(description="subtractive analysis of viral bulk RNA-seq")
+parser = argparse.ArgumentParser(description="VENUS, a subtractive analysis software: " + \
+                                             "Virus dEtecting in humaN bUlk and Single cell rna sequencing")
 
 
 ### Specifies the argument for bulk RNA-seq ###
 parser.add_argument("--read1", type=str, required=True,
-                    help="read1 of bulk RNA-seq")
+                    help="read1 of RNA-seq (barcode if single-cell)")
 parser.add_argument("--read2", type=str, required=False,
-                    help="read2 of bulk RNA-seq")
+                    help="read2 of RNA-seq (cDNA if single-cell)")
 # parser.add_argument("--indexDir", type=str, required=False, default=human_indexDir, 
 #                     help="user-specified genome index directory")
 parser.add_argument("--outDir", type=str, required=False, default=os.getcwd(), 
                     help="directory to store output")
+parser.add_argument("--singleCBstart", type=int, required=False, 
+                    help="cell barcode's start position")
+parser.add_argument("--singleCBlen", type=int, required=False,
+                    help="cell barcode's length")
+parser.add_argument("--singleUMIstart", type=int, required=False, 
+                    help="UMI's start position")
+parser.add_argument("--singleUMIlen", type=int, required=False, 
+                    help="UMI's length")
+parser.add_argument("--singleWhitelist", type=str, required=False, 
+                    help="single-cell barcode whitelist")
 args = parser.parse_args()
+
+
+### Creates a python output file ###
 
 
 ### Organizes the output directories for human then virus mappings ###
 try:
     os.chdir(args.outDir)
+    sys.stdout = open("venus.log", "x")
     print("Current working directory: {0}".format(os.getcwd()))
 except:
     print("An error has occured while changing into '{0}' directory".format(args.outDir))
@@ -43,18 +58,34 @@ except:
     sys.exit()
 
 
-### Concatenate and run the human mapping in bash ###
-if args.read2:
-    # bulk paired-end RNA seq
+
+### Firstly maps the RNA-seq reads to the human genome ###
+
+if args.singleCBstart:  # Single-Cell RNA-seq
     cmd="STAR " + \
         "--runThreadN 16 " + \
         "--outFileNamePrefix " + human_outDir + " " \
         "--genomeDir " + human_indexDir + " " \
         "--readFilesIn " + args.read2 + " " + args.read1 + " " \
         "--outReadsUnmapped Fastx " + \
-        "--outSAMtype None"
-else:
-    # bulk single-end RNA seq
+        "--outSAMtype None" + " " \
+        "--soloType CB_UMI_Simple" + " " \
+        "--soloCBwhitelist " + str(args.singleWhitelist) + " " \
+        "--soloCBstart " + str(args.singleCBstart) + " " \
+        "--soloCBlen " + str(args.singleCBlen) + " " \
+        "--soloUMIstart " + str(args.singleUMIstart) + " " \
+        "--soloUMIlen " + str(args.singleUMIlen) + " " \
+        "--soloBarcodeReadLength 0"
+
+elif args.read2:  # Bulk Paired-end RNA-seq
+        cmd="STAR " + \
+            "--runThreadN 16 " + \
+            "--outFileNamePrefix " + human_outDir + " " \
+            "--genomeDir " + human_indexDir + " " \
+            "--readFilesIn " + args.read1 + " " + args.read2 + " " \
+            "--outReadsUnmapped Fastx " + \
+            "--outSAMtype None"
+else:   # Bulk Single-end RNA-seq
     cmd="STAR " + \
         "--runThreadN 16 " + \
         "--outFileNamePrefix " + human_outDir + " " \
@@ -62,9 +93,14 @@ else:
         "--readFilesIn " + args.read1 + " " \
         "--outReadsUnmapped Fastx " + \
         "--outSAMtype None"
+
 os.system(cmd)  # Command run
 print("Running " + cmd)
 
+
+
+
+### Secondly maps the leftover reads to the viral genome ###
 
 ### Appropriately renames the read1 & read2 for the virus mapping ###
 args.read1=human_outDir + "Unmapped.out.mate1.fastq"
@@ -73,18 +109,30 @@ if args.read2:
     args.read2=human_outDir + "Unmapped.out.mate2.fastq"
     os.rename(human_outDir + "Unmapped.out.mate2", args.read2)
 
-
-### Concatenate and run the virus mapping in bash ###
-if args.read2:
-    # bulk paired-end RNA seq
+if args.singleCBstart:  # Single-Cell RNA-seq
     cmd="STAR " + \
         "--runThreadN 16 " + \
         "--outFileNamePrefix " + virus_outDir + " " \
         "--genomeDir " + virus_indexDir + " " \
         "--readFilesIn " + args.read1 + " " + args.read2 + " " \
-        "--outFilterMultimapNmax 1"
-else:
-    # bulk single-end RNA seq
+        "--outFilterMultimapNmax 1" + " " \
+        "--soloType CB_UMI_Simple" + " " \
+        "--soloCBwhitelist " + str(args.singleWhitelist) + " " \
+        "--soloCBstart " + str(args.singleCBstart) + " " \
+        "--soloCBlen " + str(args.singleCBlen) + " " \
+        "--soloUMIstart " + str(args.singleUMIstart) + " " \
+        "--soloUMIlen " + str(args.singleUMIlen) + " " \
+        "--soloBarcodeReadLength 0"
+
+elif args.read2:  # Bulk Paired-end RNA-seq
+        cmd="STAR " + \
+            "--runThreadN 16 " + \
+            "--outFileNamePrefix " + virus_outDir + " " \
+            "--genomeDir " + virus_indexDir + " " \
+            "--readFilesIn " + args.read1 + " " + args.read2 + " " \
+            "--outFilterMultimapNmax 1"
+
+else:   # Bulk Single-end RNA seq
     cmd="STAR " + \
         "--runThreadN 16 " + \
         "--outFileNamePrefix " + virus_outDir + " " \
@@ -94,6 +142,7 @@ else:
 os.system(cmd)  # Command run
 print("Running " + cmd)
 
+sys.stdout.close()
 
 # ### Counts & Output the mapping to the mega-virus index ###
 # os.chdir(args.outDir)
--- tstVenus.sh
+++ tstVenus.sh
@@ -5,8 +5,14 @@
 #SBATCH --mem-per-cpu=4G
 #SBATCH --time=24:00:00
 #SBATCH --partition=zhanglab.p
-#SBATCH --output=/srv/disk00/cheyul1/Venus/06-25-21/run4/run4.log
+#SBATCH --output=/srv/disk00/cheyul1/Venus/06-27-21/run6/run6.log
 
 python3 /srv/disk00/cheyul1/Venus/Venus.py \
---read1 /srv/disk00/cheyul1/Venus/bulk_data/single-end.fastq \
---outDir /srv/disk00/cheyul1/Venus/06-25-21/run4/
+--read1 /srv/disk00/cheyul1/Venus/data/single-cell1.fastq \
+--read2 /srv/disk00/cheyul1/Venus/data/single-cell2.fastq \
+--outDir /srv/disk00/cheyul1/Venus/06-27-21/run6/ \
+--singleCBstart 1 \
+--singleCBlen 16 \
+--singleUMIstart 17 \
+--singleUMIlen 10 \
+--singleWhitelist /srv/disk00/cheyul1/excessSTAR-2.7.8a/indices/737K-august-2016.txt
